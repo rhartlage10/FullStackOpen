f4f53ad662fbd6e9be2fa7e3573ef945059fb320
rhartlage10 <hartlage.7@wright.edu>
2021-06-03 16:10:49 -0500

    exercise 1.7 complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Button = ({ handleClick, text }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
 }
 
-export default App;
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodFeedback = () => setGood(good + 1)
+  const neutralFeedback = () => setNeutral(neutral + 1)
+  const badFeedback = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button
+        handleClick={goodFeedback}
+        text='good'
+      />
+      <Button
+        handleClick={neutralFeedback}
+        text='neutral'
+      />
+      <Button
+        handleClick={badFeedback}
+        text='bad'
+      />
+      <h2>Statistics</h2>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {good + neutral + bad}</p>
+      <p>average: {((good*1)+(bad*-1))/(good + neutral + bad)}</p>
+      <p>positive: {(good / (good + neutral + bad)) * 100} %</p>
+    </div>
+  )
+}
+
+export default App
