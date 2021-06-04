96f4eb045b329b337deaeafcd9d592b9e10f9d97
rhartlage10 <hartlage.7@wright.edu>
2021-06-04 12:34:58 -0500

    exercise 2.2 complete

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -17,9 +17,9 @@ const Header = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises + course.parts[3].exercises
   return(
-    <p>Number of exercises {sum}</p>
+    <p> <b>total of {sum} exercises </b> </p>
   ) 
 }
 
@@ -37,6 +37,7 @@ const Content = ({ course }) => {
       <Part part={course.parts[0]} />
       <Part part={course.parts[1]} />
       <Part part={course.parts[2]} />
+      <Part part={course.parts[3]} />
     </div>
   )
 }
@@ -60,11 +61,21 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
 
-  return <Course course={course} />
+  return (
+    <div>
+      <Course course={course} />
+      <Total course={course} />
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
