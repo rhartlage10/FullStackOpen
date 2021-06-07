032f3f1a3467088e05e59cc93abfd648e75d1506
rhartlage10 <hartlage.7@wright.edu>
2021-06-07 13:33:42 -0500

    practice from 2c

--- practice/part2_a/src/index.js
+++ practice/part2_a/src/index.js
@@ -1,31 +1,42 @@
 import ReactDOM from 'react-dom'
 import App from './App.js'
+// import axios from 'axios'
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
-]
+// axios
+//   .get('http://localhost:3001/notes')
+//   .then(response => {
+//     const notes = response.data
+//     console.log(notes)
+// })
 
-const result = notes.map(note => note.content)
-console.log(result)
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2)
+
+// const notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     date: '2019-05-30T17:30:31.098Z',
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only JavaScript',
+//     date: '2019-05-30T18:39:34.091Z',
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     date: '2019-05-30T19:20:14.298Z',
+//     important: true
+//   }
+// ]
+
+// const result = notes.map(note => note.content)
+// console.log(result)
 
 ReactDOM.render(
-  <App notes={notes} />,
+  <App />,
   document.getElementById('root')
 )
