59f0c7b695f95846206bbc10fe480b263d39ea0c
rhartlage10 <hartlage.7@wright.edu>
2021-06-07 13:34:49 -0500

    exercise 2.11- data fetched from json server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
+import axios from 'axios'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', num: '040-123456' },
-    { name: 'Ada Lovelace', num: '39-44-5323523' },
-    { name: 'Dan Abramov', num: '12-43-234345' },
-    { name: 'Mary Poppendieck', num: '39-23-6423122' }
+    // { name: 'Arto Hellas', num: '040-123456' },
+    // { name: 'Ada Lovelace', num: '39-44-5323523' },
+    // { name: 'Dan Abramov', num: '12-43-234345' },
+    // { name: 'Mary Poppendieck', num: '39-23-6423122' }
   ])
   const [ newName, setNewName ] = useState('')
   const [ newNum, setNewNum ] = useState('')
   const [ filter, setFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+  }, [])
+  console.log('render', persons.length, 'names')
 
   return (
     <div>
