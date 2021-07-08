ad47b64c7d27f887218b2d9314f1b0af1212a561
rhartlage10 <hartlage.7@wright.edu>
2021-07-08 10:23:39 -0500

    updated part3_a

--- practice/part3_a/index.js
+++ practice/part3_a/index.js
@@ -35,10 +35,6 @@ app.get('/api/notes', (request, response) => {
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     const note = notes.find(note => note.id === id)
-    // const note = notes.find(note => {
-    //     console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-    //     return note.id === id
-    // })
     if (note) {
         response.json(note)
     } else {
@@ -53,9 +49,37 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
+// app.post('/api/notes', (request, response) => {
+//     const note = request.body
+//     console.log(note)
+//     response.json(note)
+// })
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
 app.post('/api/notes', (request, response) => {
-    const note = request.body
-    console.log(note)
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
     response.json(note)
 })
 
