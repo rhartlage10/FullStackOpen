6d541a5759a06d334070eb8244145e8b3ad81bdb
Ronni Hartlage <hartlage.7@wright.edu>
2021-09-30 15:30:51 -0400

    6.12 - filter component created

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import App from './App'
 import store from './store'
 
-store.subscribe(() => store.getState())
+store.subscribe(() => { store.getState() })
 
 ReactDOM.render(
   <Provider store={store}>
