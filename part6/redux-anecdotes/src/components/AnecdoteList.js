419414ace05f4b581d9cb6f0f2dfbace9709cc03
Ronni Hartlage <hartlage.7@wright.edu>
2021-09-30 14:02:25 -0400

    6.10 - Notification Component added

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,10 +1,10 @@
 import React from 'react'
- import { voteAnecdote } from '../reducers/anecdoteReducer'
- import { useDispatch, useSelector } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { useDispatch, useSelector } from 'react-redux'
 
  const AnecdoteList = () => {
 
-   const anecdotes = useSelector(state => state)
+   const anecdotes = useSelector(state => state.anecdotes)
    const dispatch = useDispatch()
 
    const vote = (id) => {
