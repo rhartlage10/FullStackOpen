85dc37837e72abab86ae88228fbc15e778c34513
Ronni Hartlage <hartlage.7@wright.edu>
2021-10-03 19:59:30 -0400

    7.4 - using useField custom hook

--- part7/routed-anecdotes/src/index.js
+++ part7/routed-anecdotes/src/index.js
@@ -1,5 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import { useState } from 'react'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ export const useField = (type) => {
+   const [value, setValue] = useState('')
+
+   const onChange = (event) => {
+     setValue(event.target.value)
+   }
+
+   return {
+     type,
+     value,
+     onChange
+   }
+ }
